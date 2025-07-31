1f1f0f1d3a9245bf4dd32768170bd37633be73c4
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:14 -0600

    Add src/java/com/lam/cenicco/beans/nominas/valesdespensa/ValesDespensaBean.java 1753986193673

---
+++ src/java/com/lam/cenicco/beans/nominas/valesdespensa/ValesDespensaBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.valesdespensa;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Antonio Durán
+ */
+@Named(value = "valesDespensaBean")
+@SessionScoped
+public class ValesDespensaBean extends ValesDespensaCenicco implements Serializable{
+    
+}
