ed04c21afe0094fe85cf7825a7abf5cda3ef6ffb
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:39 -0600

    Add src/java/com/lam/cenicco/beans/nominas/reporteimpuestos/ReporteImpuestosBean.java 1753986218549

---
+++ src/java/com/lam/cenicco/beans/nominas/reporteimpuestos/ReporteImpuestosBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.reporteimpuestos;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Alejandro
+ */
+@Named(value = "reporteImpuestosBean")
+@SessionScoped
+public class ReporteImpuestosBean extends ReporteImpuestosCenicco implements Serializable {
+}
