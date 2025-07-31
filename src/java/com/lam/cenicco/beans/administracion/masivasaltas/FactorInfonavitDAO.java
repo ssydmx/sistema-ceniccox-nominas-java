9c5d7b7a4b9522cfe1b2fd98d13121492d20e9a8
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:22:54 -0600

    Add src/java/com/lam/cenicco/beans/administracion/masivasaltas/FactorInfonavitDAO.java 1753986174074

---
+++ src/java/com/lam/cenicco/beans/administracion/masivasaltas/FactorInfonavitDAO.java
@@ -0,0 +1,16 @@
+
+package com.lam.cenicco.beans.administracion.masivasaltas;
+
+/**
+ * @author erick
+ */
+public class FactorInfonavitDAO {
+     private final int LONGITUD_ARCHIVO = 3;
+    //
+    private final int POSICION_NUMERO_EMPLEADO = 0;
+    private final int POSICION_TIPO_DESCUENTO = 1;
+    private final int POSICION_IMPORTE = 2;
+    private final int POSICION_TIPO_MOVIMIENTO = 3;
+    private final int POSICION_FECHA_APLICACION = 4;
+//    
+}
