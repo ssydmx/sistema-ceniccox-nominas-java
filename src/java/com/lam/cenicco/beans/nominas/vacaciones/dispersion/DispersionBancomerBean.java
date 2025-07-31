71d2f7c34633a7138529984452a9834ee9e44d4b
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:29 -0600

    Add src/java/com/lam/cenicco/beans/nominas/vacaciones/dispersion/DispersionBancomerBean.java 1753986208817

---
+++ src/java/com/lam/cenicco/beans/nominas/vacaciones/dispersion/DispersionBancomerBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.vacaciones.dispersion;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "dispersionbancomerBean")
+@SessionScoped
+public class DispersionBancomerBean extends DispersionBancomerCenicco implements Serializable {
+}
