601890352b87784a3782f76029a32b76d6c09e9b
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:19 -0600

    Add src/java/com/lam/cenicco/beans/nominas/acumulados/consulta/AcumuladosBean.java 1753986198772

---
+++ src/java/com/lam/cenicco/beans/nominas/acumulados/consulta/AcumuladosBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.acumulados.consulta;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "acumuladosnominaBean")
+@SessionScoped
+public class AcumuladosBean extends AcumuladosCenicco implements Serializable {
+}
