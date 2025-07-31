c41847df9e7c059b25ba2a6e10d847347357124a
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:47 -0600

    Add src/java/com/lam/cenicco/beans/nominas/reportesnominas/detalleexentosgravados/DetalleExentosGravadosBean.java 1753986226846

---
+++ src/java/com/lam/cenicco/beans/nominas/reportesnominas/detalleexentosgravados/DetalleExentosGravadosBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.reportesnominas.detalleexentosgravados;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author erick
+ */
+@Named(value = "detalleExentosGravadosBean")
+@SessionScoped
+public class DetalleExentosGravadosBean extends DetalleExentosGravadosCenicco implements Serializable{
+    
+}
