757df2f2500faf686a4cfa7ad387560cbd96006e
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:29:07 -0600

    Add src/java/com/lam/cenicco/beans/nominas/polizacontable/PolizaContableBean.java 1753986546847

---
+++ src/java/com/lam/cenicco/beans/nominas/polizacontable/PolizaContableBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.polizacontable;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "polizaBean")
+@SessionScoped
+public class PolizaContableBean extends PolizaContableCenicco implements Serializable{
+    
+}
