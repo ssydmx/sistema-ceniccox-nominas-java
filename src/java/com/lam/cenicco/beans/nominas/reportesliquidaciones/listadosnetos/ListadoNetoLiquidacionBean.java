8aef347df3a196b306e45c324c3d534b0f4a002a
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:45 -0600

    Add src/java/com/lam/cenicco/beans/nominas/reportesliquidaciones/listadosnetos/ListadoNetoLiquidacionBean.java 1753986225362

---
+++ src/java/com/lam/cenicco/beans/nominas/reportesliquidaciones/listadosnetos/ListadoNetoLiquidacionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.reportesliquidaciones.listadosnetos;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author iperez
+ */
+@Named(value = "listadonetoliquidacionBean")
+@SessionScoped
+public class ListadoNetoLiquidacionBean extends ListadoNetoLiquidacionCenicco implements Serializable {
+    
+}
