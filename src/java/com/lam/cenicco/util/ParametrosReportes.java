5e4cfa50cd2b5fb9e4797fa0bb5f9a8b17045785
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:22:28 -0600

    Add src/java/com/lam/cenicco/util/ParametrosReportes.java 1753986147985

---
+++ src/java/com/lam/cenicco/util/ParametrosReportes.java
@@ -0,0 +1,208 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.util;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+public class ParametrosReportes {
+
+    public static final String MODULO = "modulo";
+    public static final String NOMBRE_ARCHIVO = "nombre";
+    public static final String TIPO_ARCHIVO = "tipoarchivo";
+    public static final String LENGUAJE = "lenguaje";
+    public static final String ARCHIVO_PDF = "pdf";
+    public static final String ARCHIVO_XML = "xml";
+    public static final String ARCHIVO_XLS = "xls";
+    public static final String ARCHIVO_XLSX = "xlsx";
+    public static final String ARCHIVO_PNG = "png";
+    public static final String ARCHIVO_TXT = "txt";
+    public static final String ARCHIVO_ALT = "alt";
+    public static final String ARCHIVO_CSV = "csv";
+    public static final String ARCHIVO_DOCX = "docx";
+    public static final String ARCHIVO_DOC = "doc";
+    public static final String ARCHIVO_ZIP = "zip";
+    public static final String REGISTROS = "registros";
+    public static final String FINIQUITO = "finiquito";
+    public static final String INDEMNIZACION = "indemnizacion";
+    public static final String REGISTROS_AUX = "registrosaux";
+    public static final String TELEFONO = "telefono";
+    public static final String SALARIO_MENSUAL = "salariomensual";
+    public static final String MAIL = "mail";
+    public static final String DOMICILIO = "domicilio";
+    public static final String RUTA = "ruta";
+//    
+    public static final String MODULO_FORMULAS = "formulas";
+    public static final String MODULO_DEPARTAMENTO = "departamento";
+    public static final String MODULO_TURNO = "turno";
+    public static final String MODULO_TIPO_RELACION_LABORAL = "tipoRelacionLaboral";
+    public static final String MODULO_GRUPO_PAGO = "grupoPago";
+    public static final String MODULO_REGISTRO_PATRONAL = "registroPatronal";
+    public static final String MODULO_COMPANIA = "compania";
+    public static final String MODULO_PUESTO = "puesto";
+    public static final String MODULO_RELACION_LABORAL = "relacionlaboral";
+    public static final String MODULO_BANCOS = "bancos";
+    public static final String MODULO_TIPO_CONTACTO = "tipoContacto";
+    public static final String MODULO_TIPO_DOCUMENTO = "tipoDocumento";
+    public static final String MODULO_TIPO_PROCESO = "tipoProceso";
+    public static final String MODULO_TIPO_PERIODO = "tipoPeriodo";
+    public static final String MODULO_CATALOGO_GENERAL = "catalogoGeneral";
+    public static final String MODULO_PAISES = "paises";
+    public static final String MODULO_ESTADOS = "estados";
+    public static final String MODULO_MUNICIPIOS = "municipios";
+    public static final String MODULO_CIUDADES = "ciudades";
+    public static final String MODULO_SALDO_VACACIONES = "saldoVacaciones";
+    public static final String MODULO_SOLICITUD_VACACIONES = "solicitudVacaciones";
+    public static final String MODULO_SOLICITUD_VACACIONES_CICLOS = "ciclosSolicitudVacaciones";
+    public static final String MODULO_SOLICITUD_VACACIONES_RESULTADO = "resultadoSolicitudVacaciones";
+    public static final String MODULO_SOLICITUD_VACACIONES_MASIVAS = "solicitudVacacionesMasivas";
+    public static final String MODULO_SOLICITUD_VACACIONES_RESULTADO_MASIVAS = "resultadosolicitudVacacionesMasivas";
+    public static final String MODULO_INCIDENCIAS = "Incidencias";
+    public static final String MODULO_SOCIOS_MOVIMIENTOS = "movimientos";
+    public static final String MODULO_SOCIOS_SALDO_DIARIO = "saldosDiario";
+    public static final String MODULO_LISTADOS_NETOS = "listadosNetos";
+    public static final String MODULO_LISTADOS_NETOS_ENTREGABLES = "listadosNetosEntregables";
+    public static final String MODULO_CARTA_FINIQUITO = "cartafiniquito";
+    public static final String MODULO_CARTA_FINIQUITO_DESGLOSADO = "cartafiniquitodesglosado";
+    public static final String MODULO_CARTA_FINIQUITO_DESGLOSADO_ALPHA = "cartafiniquitodesglosadoalpha";
+    public static final String MODULO_KIT_INICIACION_ALPHA = "kitinicioalpha";
+    public static final String MODULO_KIT_INICIACION_ALPHA_TELETRABAJO = "kitinicioalphateletrabajo";
+    public static final String MODULO_CARTA__RENUNCIA_FINIQUITO_DESGLOSADO_ALPHA = "cartarenunciafiniquitodesglosadoalpha";
+    public static final String MODULO_CARTA_EVALUACION_PRUEBA_ALPHA = "";
+    public static final String MODULO_REPORTE_TIMBRE = "reporteTimbre";
+    public static final String MODULO_REPORTE_TIMBRE_DETALLE = "reporteTimbreDetalle";
+    public static final String MODULO_CIFRAS_NOMINAS = "cifrasNominas";
+    public static final String MODULO_CIFRAS_NOMINAS_ENTREGABLES = "cifrasNominasEntregables";
+    public static final String MODULO_CIFRAS_NOMINAS_GRAVADOS_EXENTOS = "cifrasNominasGravadosExentos";
+    public static final String MODULO_LIQUIDACIONES_CIFRAS_NOMINAS = "liquidacionesCifrasNominas";
+    public static final String MODULO_LIQUIDACIONES_LISTADOS_NETOS = "liquidacionesListadosNetos";
+    public static final String MODULO_LIQUIDACIONES_LISTADOS_NOMINAS = "liquidacionesListadosNominas";
+    public static final String MODULO_GRUPO_CONCEPTO = "grupoConcepto";
+    public static final String MODULO_CONCEPTOS = "conceptos";
+    public static final String MODULO_POLIZA_CONTABLE = "polizaContable";
+    public static final String MODULO_POLIZA = "poliza";
+    public static final String MODULO_LIQUIDACIONES = "liquidaciones";
+    public static final String MODULO_LISTADO_NOMINAS = "listadoNominas";
+    public static final String MODULO_LISTADO_NOMINAS_ENTREGABLES = "listadoNominasEntregables";
+    public static final String MODULO_LISTADO_NOMINAS_ENTREGABLES_ORDAS = "listadoNominasEntregablesordas";
+    public static final String MODULO_LISTADO_NOMINAS_ENTREGABLES_X_DEPTO = "listadoNominasEntregablesxdepto";
+    public static final String MODULO_LISTADO_NOMINAS_PTU = "listadoNominasPtu";
+    public static final String MODULO_LISTADO_NOMINAS_SUBREPORTE = "listadoNominasSubReporte";
+    public static final String MODULO_CONSTANCIAS_SAT = "constanciasSAT";
+    public static final String MODULO_FINIQUITOS = "finiquitos";
+    public static final String MODULO_DISPERSION_BANCARIA = "dispersionBancaria";
+    public static final String MODULO_MENSUAL_EMPLEADO_CONCEPTO = "mensualempleadoconcepto";
+    public static final String MODULO_MENSUAL_CONCEPTO = "mensualconcepto";
+    public static final String MODULO_IMPUESTOS = "reporteimpuestos";
+    public static final String MODULO_CARTA_PATRONAL_IMSS = "cartapatronalimss";
+    public static final String MODULO_CARTA_LABORAL = "cartalaboral";
+    public static final String MODULO_CK_CARTA_LABORAL_KONECTA = "cartalaboralkonecta";
+    public static final String MODULO_CARTA_FONACOT = "cartafonacot";
+    public static final String MODULO_CARTA_NO_AJUSTE = "cartanoajuste";
+    public static final String MODULO_CARTA_DE_RENUNCIA = "cartarenuncia";
+    public static final String MODULO_CARTA_DE_RENUNCIA_ALPHA = "cartarenunciaalpha";
+    public static final String MODULO_CARTA_DE_RENUNCIA_WHITEHAT = "cartarenunciawhitehat";
+    public static final String MODULO_CARTA_DE_EMBAJADA = "cartaembajada";
+    public static final String MODULO_CARTA_DE_GUARDERIA = "cartaguarderia";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO = "cartatiempodeterminado";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_ABASTOHOTEL = "cartatiempodeterminadoabastohotel";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO = "cartatiempoindeterminado";
+    public static final String MODULO_CARTA_DE_CONVENIO_CONFIDENCILIDAD = "cartaconvenioconfidencialidad";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO_NUTRITION = "cartatiempoindeterminadonutrition";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO_ENERFENIX = "cartatiempoindeterminadoenerfenix";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_COVID = "cartatiempodeterminadocovid";
+    public static final String MODULO_CARTA_DE_PERIODO_PRUEBA_WHITEHAT = "cartaperiodopruebawhitehat";
+    public static final String MODULO_CARTA_DE_RECOMENDACION_YAMANA = "cartarecomendacionyamana";
+    public static final String MODULO_CARTA_PROGRESS = "cartaprogress";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_NUTRITION = "cartatiempodeterminadonutrition";
+    public static final String MODULO_CARTA_DE_CONVENIO_CONFIDENCILIDAD_NUTRITION = "cartaconvenioconfidencialidadnutrition";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_ADMINISTRATIVO_ARTSANA = "cartatiempodeterminadoadministrativoartsana";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_BECARIO_ARTSANA = "cartatiempodeterminadobecarioartsana";
+    public static final String MODULO_CARTA_DE_TIEMPO_DETERMINADO_VENTAS_ARTSANA = "cartatiempodeterminadoventasartsana";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO_ADMINISTRATIVO_ARTSANA = "cartatiempoindeterminadoadministrativoartsana";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO_BECARIO_ARTSANA = "cartatiempoindeterminadobecarioartsana";
+    public static final String MODULO_CARTA_DE_TIEMPO_INDETERMINADO_VENTAS_ARTSANA = "cartatiempoindeterminadoventasartsana";
+    public static final String MODULO_CARTA_DE_RENUNCIA_KUA = "cartaderenunciakua";
+    public static final String MODULO_CONTRATO_PRESTACION_SERVICIOS_PROFESIONALES = "contratoprestacionserviciosprofesionales";
+    public static final String MODULO_CREDENCIAL_DELMONTE = "credencialdelmonte";
+    public static final String MODULO_CONTRATO_DELMONTE_COSECHADOR = "contratodelmontecosechador";
+    public static final String MODULO_CONTRATO_DELMONTE_COSECHADOR_BONO = "contratodelmontecosechadorbono";
+    public static final String MODULO_CONTRATO_DELMONTE_MAYORDOMO = "contratodelmontemayordomo";
+    public static final String MODULO_CONTRATO_DELMONTE_MAYORDOMO_BONO = "contratodelmontemayordomobono";
+    public static final String MODULO_CONTRATO_DELMONTE_SORTEADOR = "contratodelmontesorteador";
+    public static final String MODULO_CONTRATO_DELMONTE_SORTEADOR_BONO = "contratodelmontesorteadorbono";
+    public static final String MODULO_CONTRATO_DELMONTE_OPERATIVO = "contratodelmonteoperativo";
+    public static final String MODULO_ASIGNACION_HERRAMIENTA_TRABAJO = "asignacionherramientatrabajo";
+    public static final String MODULO_ENTREGA_EQUIPO_RESGUARDO = "entregaequiporesguardo";
+    public static final String MODULO_CARTA_PATRONAL_ARTSANA = "cartapatronalartsana";
+    public static final String MODULO_CARTA_RENUNCIA_ARTSANA = "cartarenunciaartsana";
+    public static final String MODULO_CARTA_ENTREGA_TARJETA_NOMINA = "cartaentregatarjetanomina";
+    public static final String MODULO_LIBERACION_ARTSANA = "cartaliberacionartsana";
+    public static final String MODULO_CONSTANCIA_LABORAL_MINNT = "constancialaboralminnt";
+    public static final String MODULO_POLITICAS_USO_UNIFORME = "politicasusouniforme";
+    public static final String MODULO_POLITICAS_GENERALES_ADMINITRATIVOS_ARTSANA = "politicasgeneralesadminartsana";
+    public static final String MODULO_POLITICAS_TIENDAS_ARTSANA = "politicastiendasartsana";
+    public static final String REPORTE_NOMINA_COMPARATIVA = "reporteNominaComparativa";
+    public static final String MODULO_CARTA_CONVENIO_FINIQUITO_YAMANA = "cartaconveniofiniquitoyamana";
+    public static final String MODULO_RECIBO_GENERICO_FINIQUITO = "recibogenericofiniquito";
+    public static final String MODULO_CONVENIO_TERMINACION = "convenioTerminacionW";
+    public static final String MODULO_CONVENIO_TERMINACIONWEN = "convenioTerminacionWEn";
+//    
+//    
+    public static final String PARAMETRO_FECHA_INGRESO = "fechaIngreso";
+    public static final String PARAMETRO_FECHA_ANTIGUEDAD = "fechaAntiguedad";
+    public static final String PARAMETRO_CANTIDAD_VACACIONES_SOLICITADAS = "vacacionesSolicitadas";
+    public static final String PARAMETRO_CANTIDAD_VACACIONES_DEVENGADAS = "vacacionesDevengadas";
+    public static final String PARAMETRO_CANTIDAD_VACACIONES_AUTORIZADAS = "vacacionesAutorizadas";
+    public static final String PARAMETRO_SALDO_VACACIONES = "saldoVacaciones";
+//
+    public static final String PARAMETRO_FECHAS_DUPLICADAS_VACACIONES_RESULTADO = "fechasDuplicadas";
+    public static final String PARAMETRO_REGISTRO_EXITOSO_VACACIONES_RESULTADO = "registrosExitosos";
+    public static final String PARAMETRO_SALDO_INSUFICIENTE_VACACIONES_RESULTADO = "saldoInsuficiente";
+    public static final String PARAMETRO_TOTAL_REGISTROS_VACACIONES_RESULTADO = "totalRegistros";
+    public static final String PARAMETRO_BANCO = "banco";
+    public static final String PARAMETRO_GRUPO_PAGO = "grupopago";
+    public static final String PARAMETRO_TIPO_NOMINA = "tiponomina";
+    public static final String PARAMETRO_PERIODO = "periodo";
+    public static final String PARAMETRO_FECHA_PAGO = "fechapago";
+    public static final String PARAMETRO_TOTAL = "total";
+    public static final String PARAMETRO_TOTAL_PERCEPCIONES = "totalpercepciones";
+    public static final String PARAMETRO_TOTAL_DEDUCCIONES = "totaldeducciones";
+    public static final String PARAMETRO_TOTAL_CARGO = "totalcargo";
+    public static final String PARAMETRO_TOTAL_ABONO = "totalbono";
+    public static final String PARAMETRO_COMPANIA = "compania";
+    public static final String PARAMETRO_LOGO = "logo";
+//    
+    public static final String PARAMETRO_IMPORTE_GRAVADO = "importegravado";
+    public static final String PARAMETRO_IMPORTE_EXENTO = "importeexento";
+//    
+    public static final String MODULO_GO_CARTA_LABORAL = "cartaLaboral";
+    public static final String MODULO_GO_CARTA_IMSS = "cartaImss";
+    public static final String MODULO_GO_CARTA_EMBAJADA = "cartaEmbajada";
+    public static final String MODULO_GO_CARTA_BANCO_BBVA = "cartaBancoBBVA";
+    public static final String MODULO_GO_CARTA_FONACOT_GOH = "cartaFonacotGOH";
+    public static final String MODULO_GO_CARTA_FONACOT_SO = "cartaFonacotSO";
+    public static final String MODULO_GO_CARTA_MATERNIDAD = "cartaMaternidad";
+    public static final String MODULO_GO_CARTA_GUARDERIA = "cartaGuarderia";
+    public static final String MODULO_GO_SOLICITUD_VACACIONES = "solicitudVacaciones";
+//
+    public static final String MODULO_CK_CONTRATO_TRABAJO_TELETRABAJO = "contratoTrabajoTeletrabajo";
+    public static final String MODULO_CK_CONTRATO_TRABAJO_GROWTH_PRECENCIAL = "contratoTrabajoGrowthPrecencial";
+    public static final String MODULO_CK_CONTRATO_CONFIDENCIALIDAD_NDA = "contratoConfidencialidadNda";
+    public static final String MODULO_CK_CONSTANCIA_LABORAL_ACTIVO = "constanciaLaboralActivo";
+    public static final String MODULO_CK_CONTRATO_TRABAJO_INDETERMINADO_MODALIDAD_PRUEBA = "contratoConektaTrabajoPresencial";
+    public static final String MODULO_CK_CONTRATO_TRABAJO_GROWTH_TELETRABAJO = "contratoTrabajoGrowthTeletrabajo";
+    public static final String MODULO_CK_CONSTANCIA_LABORAL_BAJA = "constanciaLaboralBaja";
+    public static final String PUBLICMODULO_CK_CARTA_MATERNIDAD = "cartaMaternidadConekta";
+    public static final String MODULO_CONTRATO_TELETRABAJO_OPERATIVO = "contratoTeletrabajoOperativo";
+    public static final String MODULO_CONTRATO_TELETRABAJO_ADMIN = "ContratoTeletrabajoAdmino";
+    public static final String MODULO_POLITICA_TELETRABAJO_MODALIDAD_HIBRIDA = "politicaTeletrabajoModalidadHibrida";
+    public static final String MODULO_ADDENDUM_CONTRATO_TELETRABAJO = "addendumContratoTeletrabajo";
+    public static final String MODULO_CARTA_RENUNCIA = "cartaRenuncia";
+    public static final String MODULO_SOLICITUD_PAGO = "solicitudPago";
+    public static final String MODULO_CARTA_PARTES_PROMOCIONALES_FINIQUITO = "cartaPartesPromocionalesFiniquito";
+    public static final String MODULO_ZIP_DOCUMENTACION_FINIQUITOS = "zipDocumentacionFiniquitos";
+}
