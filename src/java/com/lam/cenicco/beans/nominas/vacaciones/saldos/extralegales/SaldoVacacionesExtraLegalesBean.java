b0e3e7df98de50dac6721b7acbbf81bb827e7756
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:24 -0600

    Add src/java/com/lam/cenicco/beans/nominas/vacaciones/saldos/extralegales/SaldoVacacionesExtraLegalesBean.java 1753986203756

---
+++ src/java/com/lam/cenicco/beans/nominas/vacaciones/saldos/extralegales/SaldoVacacionesExtraLegalesBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.vacaciones.saldos.extralegales;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Cenicco
+ */
+@Named(value = "saldoVacacionesExtraLegalesBean")
+@SessionScoped
+public class SaldoVacacionesExtraLegalesBean extends SaldoVacacionesExtraLegalesCenicco implements Serializable {
+    
+}
