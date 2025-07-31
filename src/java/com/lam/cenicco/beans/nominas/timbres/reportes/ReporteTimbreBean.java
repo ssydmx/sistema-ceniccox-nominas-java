d3aecd22fcaf772ffbf9c1fbb9d03432d6196844
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:33 -0600

    Add src/java/com/lam/cenicco/beans/nominas/timbres/reportes/ReporteTimbreBean.java 1753986212782

---
+++ src/java/com/lam/cenicco/beans/nominas/timbres/reportes/ReporteTimbreBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.timbres.reportes;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "reportetimbreBean")
+@SessionScoped
+public class ReporteTimbreBean extends ReporteTimbreCenicco implements Serializable {
+}
