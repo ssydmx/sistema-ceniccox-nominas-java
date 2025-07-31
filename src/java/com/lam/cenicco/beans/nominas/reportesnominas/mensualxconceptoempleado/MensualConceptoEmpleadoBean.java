9d473e3f40e47a36c6a3f796fb418688183e5159
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:39 -0600

    Add src/java/com/lam/cenicco/beans/nominas/reportesnominas/mensualxconceptoempleado/MensualConceptoEmpleadoBean.java 1753986219450

---
+++ src/java/com/lam/cenicco/beans/nominas/reportesnominas/mensualxconceptoempleado/MensualConceptoEmpleadoBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.reportesnominas.mensualxconceptoempleado;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Antonio Durán
+ */
+@Named(value = "mensualxconceptoempleadoBean")
+@SessionScoped
+public class MensualConceptoEmpleadoBean extends MensualConceptoEmpleadoCenicco implements Serializable{
+    
+}
