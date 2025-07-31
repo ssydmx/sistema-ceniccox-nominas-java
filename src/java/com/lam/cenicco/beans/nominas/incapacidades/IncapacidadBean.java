13d15f8b8a37c097a082ddddb041630b80601ad9
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:37:46 -0600

    Add src/java/com/lam/cenicco/beans/nominas/incapacidades/IncapacidadBean.java 1753987065763

---
+++ src/java/com/lam/cenicco/beans/nominas/incapacidades/IncapacidadBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.incapacidades;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "incapacidadBean")
+@SessionScoped
+public class IncapacidadBean extends IncapacidadCenicco implements Serializable{
+    
+}
