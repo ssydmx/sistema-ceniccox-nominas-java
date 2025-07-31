36b8029e52b22776526ca39d102b0c6495de6ea7
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:50 -0600

    Add src/java/com/lam/cenicco/beans/nominas/reportesnominas/conceptoxperiodo/ConceptoxPeriodoBean.java 1753986230435

---
+++ src/java/com/lam/cenicco/beans/nominas/reportesnominas/conceptoxperiodo/ConceptoxPeriodoBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.reportesnominas.conceptoxperiodo;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "conceptoxperiodoBean")
+@SessionScoped
+public class ConceptoxPeriodoBean extends ConceptoxPeriodoCenicco implements Serializable {
+}
