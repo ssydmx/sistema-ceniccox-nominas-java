3110014dd9fcb90dff0b7d9db5564b83084f44e9
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:35 -0600

    Add src/java/com/lam/cenicco/beans/nominas/saldocreditos/SaldoCreditosBean.java 1753986215537

---
+++ src/java/com/lam/cenicco/beans/nominas/saldocreditos/SaldoCreditosBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.saldocreditos;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Antonio Durán
+ */
+@Named(value = "saldocreditosBean")
+@SessionScoped
+public class SaldoCreditosBean extends SaldoCreditosCenicco implements Serializable {
+}
