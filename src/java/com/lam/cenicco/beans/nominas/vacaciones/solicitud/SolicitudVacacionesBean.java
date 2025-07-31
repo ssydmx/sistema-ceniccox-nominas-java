5b847bacb6c4298854e8b0d4405fe8fc7894d4c1
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:23:20 -0600

    Add src/java/com/lam/cenicco/beans/nominas/vacaciones/solicitud/SolicitudVacacionesBean.java 1753986199703

---
+++ src/java/com/lam/cenicco/beans/nominas/vacaciones/solicitud/SolicitudVacacionesBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.vacaciones.solicitud;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Antonio Durán
+ */
+@Named(value = "solicitudvacacionesBean")
+@SessionScoped
+public class SolicitudVacacionesBean extends SolicitudVacacionesCenicco implements Serializable {
+}
