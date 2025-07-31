4ccfb8ab691d86b8a076d2b3254342e24b47e3e0
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:32:07 -0600

    Add src/java/com/lam/cenicco/beans/nominas/ptu/calculo/PtuBean.java 1753986726855

---
+++ src/java/com/lam/cenicco/beans/nominas/ptu/calculo/PtuBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.nominas.ptu.calculo;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Antonio Durán
+ */
+@Named(value = "ptuBean")
+@SessionScoped
+public class PtuBean extends PtuCenicco implements Serializable{
+    
+}
