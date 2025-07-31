c4d5c7607461c464aae672e3c9a727549ad4e66b
ssydmx <129028512+ssydmx@users.noreply.github.com>
2025-07-31 12:22:33 -0600

    Add src/java/com/lam/cenicco/beans/administracion/tiposalarioidse/TipoSalarioIdseBean.java 1753986153337

---
+++ src/java/com/lam/cenicco/beans/administracion/tiposalarioidse/TipoSalarioIdseBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lam.cenicco.beans.administracion.tiposalarioidse;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author JoséAntonio
+ */
+@Named(value = "tipoSalarioIdseBean")
+@SessionScoped
+public class TipoSalarioIdseBean extends TipoSalarioIdseCenicco implements Serializable {
+}
